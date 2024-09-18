8f4fe24634f91636a385e6c848b1fbd94739197c
Ulugbek <abdusamatovulugbekofficial@gmail.com>
2024-09-18 20:11:34 +0500

    news

--- 11. Tsikl/js/script.js
+++ 11. Tsikl/js/script.js
@@ -2,3 +2,57 @@
 
 
 
+//birinchi yo'li
+
+// let startNum = 1;
+
+// while (startNum <= 10) {
+//     console.log(startNum);
+//     startNum++;
+// }
+
+
+
+//ikkinchi yo'li
+
+// let startNum = 1;
+
+// do {
+//     console.log(startNum);
+//     startNum++;
+// } while (startNum <= 20);
+
+
+
+//uchinchi yo'li
+
+// for (let i = 1; i <= 30; i++) {
+//     console.log(i);
+// }
+
+
+
+// let num = 10;
+
+// for (let i = 1; i <= 10; i++) {
+//     console.log(num);
+//     num++;
+// }
+
+
+
+// for (let i = 1; i <= 20; i++) {
+//     if (i === 15) {
+//         break;
+//     }
+//     console.log(i);
+// }
+
+
+
+for (let i = 1; i <= 10; i++) {
+    if (i === 7) {
+        continue;
+    }
+    console.log(i);
+}
